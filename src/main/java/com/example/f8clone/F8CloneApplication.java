496e29a66dca3a3301349ae8a3accb77b1fe80e3
Kienbeo237 <kiennhhe163054@fpt.edu.vn>
2022-12-06 21:02:52 +0700

    create project

---
+++ src/main/java/com/example/f8clone/F8CloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.f8clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F8CloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(F8CloneApplication.class, args);
+    }
+
+}
